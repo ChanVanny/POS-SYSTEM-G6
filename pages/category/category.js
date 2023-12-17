61abe8cd13e7e244fe4f37f9c44c10422c3ce6aa
VANNY.CHAN <VANNY.CHAN@pnc.lan>
2023-12-17 15:35:52 +0700

    #24 search category

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,9 +1,9 @@
 
-
-let buttonAddCategory = document.querySelector('.add-category');
+let buttonAddCategory = document.querySelector('.add-btn');
 let categoryForm = document.querySelector('.category-form');
 let showCategory = document.querySelector('.show-category');
 let buttonView = document.querySelector('.button-view');
+let inputSearch = document.querySelector('#search');
 
 // Add Category
 const inputName = document.querySelector('#name');
@@ -12,19 +12,19 @@ const saveButton = document.querySelector('#save');
 const updateButton = document.querySelector('#update');
 const table = document.querySelector('table');
 
-let info = [
+let categories = [
     { id: 1, name: "Drink", description: "test" },
     { id: 2, name: "Cake", description: "test" },
     { id: 3, name: "Fruit", description: "test" },
 ];
 
 function saveStorage() {
-    localStorage.setItem("info", JSON.stringify(info));
+    localStorage.setItem("categories", JSON.stringify(categories));
 }
 
 function getStorage() {
-    if (JSON.parse(localStorage.getItem("info")) != null) {
-        info = JSON.parse(localStorage.getItem("info"));
+    if (JSON.parse(localStorage.getItem("categories")) != null) {
+        categories = JSON.parse(localStorage.getItem("categories"));
     }
 }
 
@@ -42,10 +42,21 @@ function viewCategory() {
 }
 
 function showCategories() {
+    let searchText = inputSearch.value;
+    let categoryList = categories
+    if(searchText != ""){
+        categoryList=[]
+        for(let category of categories){
+            if(category.name.toLowerCase().indexOf(searchText.toLowerCase())>-1){
+                categoryList.push(category)
+            }
+        }
+    }
     const tbody = document.querySelector('tbody');
     tbody.remove();
     let tBody = document.createElement('tbody');
-    for (let index = 0; index < info.length; index++) {
+
+    for (let index = 0; index < categoryList.length; index++) {
         let tr = document.createElement('tr');
         let tdId = document.createElement('td');
         let tdName = document.createElement('td');
@@ -56,8 +67,8 @@ function showCategories() {
         let iDelete = document.createElement('i');
         let iEdit = document.createElement('i');
 
-        tdId.textContent = info[index].id;
-        tdName.textContent = info[index].name;
+        tdId.textContent = categoryList[index].id;
+        tdName.textContent = categoryList[index].name;
 
         iDelete.className = "ri-delete-bin-6-line";
         iEdit.className = "ri-pencil-fill";
@@ -84,7 +95,7 @@ function showCategories() {
 }
 function deleteCategory(e) {
     const index = e.target.parentElement.parentElement.dataset.index
-    info.splice(index, 1)
+    categories.splice(index, 1)
     saveStorage()
     getStorage()
     showCategories()
@@ -99,12 +110,12 @@ function update(e) {
     e.preventDefault()
     let index = localStorage.getItem("index")
     let category = {
-        id: info[index].id,
+        id: categories[index].id,
         name: inputName.value,
         description: description.value
     }
     if (inputName.value != "" && description.value != "") {
-        info.splice(index, 1, category);
+        categories.splice(index, 1, category);
         saveStorage();
         getStorage();
         showCategories();
@@ -116,8 +127,8 @@ function editCategory(e) {
     const index = e.target.parentElement.parentElement.dataset.index;
     localStorage.setItem("index", index);
     showForm();
-    inputName.value = info[index].name;
-    description.value = info[index].description;
+    inputName.value = categories[index].name;
+    description.value = categories[index].description;
     saveButton.classList.add('hide');
     updateButton.classList.remove('hide');
     updateButton.addEventListener('click', update);
@@ -126,25 +137,24 @@ function editCategory(e) {
 function addCategory(e) {
     e.preventDefault();
     let newCate = {
-        id: info.length + 1,
+        id: categories.length + 1,
         name: inputName.value,
         description: description.value
     };
     if (inputName.value != "" && description.value != "") {
-        info.push(newCate);
+        categories.push(newCate);
         viewCategory();
         saveStorage();
         getStorage();
         showCategories();
-    }
-    
-
+    }    
 }
 
 // Add EventListener
 saveButton.addEventListener('click', addCategory)
 buttonAddCategory.addEventListener('click', showForm)
 buttonView.addEventListener('click', viewCategory);
+inputSearch.addEventListener('keyup', showCategories);
 
 // Call Function
 // saveStorage()
