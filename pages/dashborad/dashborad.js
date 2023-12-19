b3b62d40c52760ba1a363b4302f1963947d2f226
DANY.NAY <DANY.NAY@PNC.LAN>
2023-12-19 18:51:54 +0700

    #30 update dashborad

--- pages/dashborad/dashborad.js
+++ pages/dashborad/dashborad.js
@@ -1,14 +1,15 @@
 let productstorage = JSON.parse(localStorage.getItem('datass'))
 
 if (JSON.parse(localStorage.getItem("categories")) != null) {
-    let categories = JSON.parse(localStorage.getItem("categories"));
+    categories = JSON.parse(localStorage.getItem("categories"));
  
 }
 
 
-
 let instock = document.querySelector('#stock1');
 let Category = document.querySelector('#stock2');
+
+
 
 instock.textContent =productstorage.length;
 Category.textContent=categories.length;
