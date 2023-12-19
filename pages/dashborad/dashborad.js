b9f2d50a9f3b5612222b1f85c856cbe03ad4fbb1
DANY.NAY <DANY.NAY@PNC.LAN>
2023-12-19 12:40:07 +0700

    #30 to take data from other local

--- pages/dashborad/dashborad.js
+++ pages/dashborad/dashborad.js
@@ -1,5 +1,16 @@
 let productstorage = JSON.parse(localStorage.getItem('datass'))
 
+if (JSON.parse(localStorage.getItem("categories")) != null) {
+    categories = JSON.parse(localStorage.getItem("categories"));
+ 
+}
+
+
 
 let instock = document.querySelector('#stock1');
+let Category = document.querySelector('#stock2');
+
 instock.textContent =productstorage.length;
+Category.textContent=categories.length;
+
+
